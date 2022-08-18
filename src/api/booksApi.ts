171baddce5edad73557ae7f207693250f65cf237
kisnj <knandor407@gmail.com>
2022-08-18 11:08:51 +0200

    new fetch lcoation

--- src/api/booksApi.ts
+++ src/api/booksApi.ts
@@ -1,6 +1,6 @@
 import { useUserStore } from "../stores/userStore";
 export const getIndexPage = async () => {
-  const res = fetch("http://localhost:3100/api");
+  const res = fetch("https://protected-plains-00046.herokuapp.com/api");
   const data = (await res).json();
   return data;
 };
@@ -10,15 +10,18 @@ interface formDataSignUp {
   confirmPassword: string;
 }
 export const signUp = async (formData: formDataSignUp) => {
-  const response = await fetch("http://localhost:3100/signup", {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
+  const response = await fetch(
+    "https://protected-plains-00046.herokuapp.com/signup",
+    {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify(formData),
     },
-    credentials: "include",
-    body: JSON.stringify(formData),
-  });
+  );
   return response;
 };
 interface formDatalogIn {
@@ -26,24 +29,30 @@ interface formDatalogIn {
   password: string;
 }
 export const logIn = async (formDataLogin: formDatalogIn) => {
-  const response = await fetch("http://localhost:3100/login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
+  const response = await fetch(
+    "https://protected-plains-00046.herokuapp.com/login",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify(formDataLogin),
     },
-    credentials: "include",
-    body: JSON.stringify(formDataLogin),
-  });
+  );
   return response;
 };
 export const logOut = async () => {
-  const response = await fetch("http://localhost:3100/logout", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
+  const response = await fetch(
+    "https://protected-plains-00046.herokuapp.com/logout",
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
     },
-    credentials: "include",
-  });
+  );
   return response;
 };
 interface IformDataCreatePost {
@@ -52,25 +61,31 @@ interface IformDataCreatePost {
   content: string;
 }
 export const postBook = async (formDataCreatePost: IformDataCreatePost) => {
-  const response = await fetch("http://localhost:3100/api/blogs", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
+  const response = await fetch(
+    "https://protected-plains-00046.herokuapp.com/api/blogs",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify(formDataCreatePost),
     },
-    credentials: "include",
-    body: JSON.stringify(formDataCreatePost),
-  });
+  );
   return response;
 };
 
 export const getEveryPublicPost = async () => {
-  const response = await fetch("http://localhost:3100/api/blogs", {
-    method: "GET",
-    credentials: "include",
-    headers: {
-      Accept: "application/json",
+  const response = await fetch(
+    "https://protected-plains-00046.herokuapp.com/api/blogs",
+    {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        Accept: "application/json",
+      },
     },
-  });
+  );
   return response;
 };
 export interface IAuthor {
@@ -97,20 +112,23 @@ export interface IBlog {
 }
 
 export const getBlogById = async (id: string) => {
-  const response = await fetch(`http://localhost:3100/api/blogs/${id}`, {
-    method: "GET",
-    headers: {
-      Accept: "application/json",
+  const response = await fetch(
+    `https://protected-plains-00046.herokuapp.com/api/blogs/${id}`,
+    {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+      },
+      credentials: "include",
     },
-    credentials: "include",
-  });
+  );
   return response;
 };
 
 export const addComment = async (blog_id: string, content: string) => {
   console.log(JSON.stringify(content));
   const response = await fetch(
-    `http://localhost:3100/api/blogs/${blog_id}/comment`,
+    `https://protected-plains-00046.herokuapp.com/api/blogs/${blog_id}/comment`,
     {
       method: "POST",
       headers: {
@@ -126,7 +144,7 @@ export const addComment = async (blog_id: string, content: string) => {
 };
 export const deleteComment = async (blog_id: string, comment_id: string) => {
   const response = await fetch(
-    `http://localhost:3100/api/blogs/${blog_id}/comment/${comment_id}`,
+    `https://protected-plains-00046.herokuapp.com/api/blogs/${blog_id}/comment/${comment_id}`,
     {
       method: "DELETE",
       headers: {
@@ -143,7 +161,7 @@ export const updateComment = async (
   content: string,
 ) => {
   const response = await fetch(
-    `http://localhost:3100/api/blogs/${blog_id}/comment/${comment_id}`,
+    `https://protected-plains-00046.herokuapp.com/api/blogs/${blog_id}/comment/${comment_id}`,
     {
       method: "PUT",
       headers: {
@@ -157,14 +175,17 @@ export const updateComment = async (
   return response;
 };
 export const deleteBlog = async (blog_id: string) => {
-  const response = await fetch(`http://localhost:3100/api/blogs/${blog_id}`, {
-    method: "DELETE",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
+  const response = await fetch(
+    `https://protected-plains-00046.herokuapp.com/api/blogs/${blog_id}`,
+    {
+      method: "DELETE",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
     },
-    credentials: "include",
-  });
+  );
   return response;
 };
 export interface IUpdateBlog {
@@ -173,24 +194,30 @@ export interface IUpdateBlog {
   public: boolean;
 }
 export const updateBlog = async (blog_id: string, content: IUpdateBlog) => {
-  const response = await fetch(`http://localhost:3100/api/blogs/${blog_id}`, {
-    method: "PUT",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
+  const response = await fetch(
+    `https://protected-plains-00046.herokuapp.com/api/blogs/${blog_id}`,
+    {
+      method: "PUT",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({ content }),
     },
-    credentials: "include",
-    body: JSON.stringify({ content }),
-  });
+  );
   return response;
 };
 export const getEveryOwnPost = async () => {
-  const response = await fetch("http://localhost:3100/api/blogs/own", {
-    method: "GET",
-    credentials: "include",
-    headers: {
-      Accept: "application/json",
+  const response = await fetch(
+    "https://protected-plains-00046.herokuapp.com/api/blogs/own",
+    {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        Accept: "application/json",
+      },
     },
-  });
+  );
   return response;
 };
